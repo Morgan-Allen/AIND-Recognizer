84c16131046f02b641c138680e7e933f6d52ed81
Morgan Allen <morganallen@Macintosh.local>
2017-05-09 11:24:01 +0100

    Added some explicit notes/comments on superclass implementation and some fixes to ensure consistent score-selection.

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -77,22 +77,22 @@ class ModelSelector(object):
             return None
     
     def select(self):
-        picked = None
-        best_score = float("inf")
+        best_model = None
+        best_score = float("-inf")
         
         for p in range(self.min_n_components, self.max_n_components):
             try:
                 model = self.base_model(p)
                 score = self.score_model(model)
-                if score < best_score:
-                    picked = model
+                if score > best_score:
+                    best_model = model
                     best_score = score
             except Exception as e:
                 print("  Selection problem:", e, "for", self.this_word)
                 continue
         
-        self.report_selection(picked)
-        return picked
+        self.report_selection(best_model)
+        return best_model
     
     def report_selection(self, model):
         word = self.this_word
@@ -124,13 +124,18 @@ class SelectorBIC(ModelSelector):
     """ select the model with the lowest Baysian Information Criterion(BIC) score
     http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
     Bayesian information criteria: BIC = -2 * logL + p * logN
+    
+    NOTE:  Actual model-selection is performed in the ModelSelector superclass-
+    this method simply scores a pre-generated model.  (The BIC formula is also
+    negated, so that the model with *highest* score can be selected.)
     """
     def score_model(self, model):
-        num_S, num_F = model.n_components, len(model.means_[0])
-        num_params = (num_S - 1) + (num_S * (num_S - 1)) + (2 * num_S * num_F)
-        acc_bonus   = -2 * model.score(self.X, self.lengths)
-        len_penalty = num_params * math.log(len(self.lengths))
-        return acc_bonus + len_penalty
+        num_D, num_F = self.X.shape
+        num_S        = model.n_components
+        num_params   = (num_S - 1) + (num_S * (num_S - 1)) + (2 * num_S * num_F)
+        acc_bonus    = 2 * model.score(self.X, self.lengths)
+        size_penalty = num_params * math.log(num_D)
+        return acc_bonus - size_penalty
 
 
 class SelectorDIC(ModelSelector):
@@ -140,6 +145,9 @@ class SelectorDIC(ModelSelector):
     Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+    
+    NOTE:  Actual model-selection is performed in the ModelSelector superclass-
+    this method simply scores a pre-generated model.
     '''
     def score_model(self, model):
         self_score = model.score(self.X, self.lengths)
@@ -150,15 +158,20 @@ class SelectorDIC(ModelSelector):
             X, lengths = self.hwords[word]
             sum_other_scores += model.score(X, lengths)
         
-        return (sum_other_scores / (len(self.hwords) - 1)) - self_score
+        return self_score - (sum_other_scores / (len(self.hwords) - 1))
 
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds.
+    
+    NOTE:  Actual model-selection is performed in the ModelSelector superclass-
+    this method simply scores a pre-generated model.
     '''
     split = None
     
     def score_model(self, model):
+        #  NOTE:  KFold() chokes on small sequences, so I've hard-coded some
+        #  default-splits in these cases:
         if self.split == None:
             if   len(self.sequences) == 1: self.split = [([0], [0])]
             elif len(self.sequences) == 2: self.split = [([0], [1]), ([1], [0])]
